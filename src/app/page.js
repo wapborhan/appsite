dc57d4272179d14c337605518570fdd03e552bc9
Borhan Uddin <68281712+wapborhan@users.noreply.github.com>
2024-03-30 20:12:26 +0600

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -20,6 +20,7 @@ export default function Home() {
       </div>
 
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 ">
+        Developed
         <a
           href="https://www.srdreamlab.com"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
@@ -27,7 +28,7 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Developed{" "}
+            SR Dream lab
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
